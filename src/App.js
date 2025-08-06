237d1a2dff438c2aabda4492191bb23406a40290
unknown <johan_nordli@hotmail.com>
2025-08-06 16:01:05 +0200

    Satt upp en site

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-        {count}
-        <button onClick={() => setCount(count + 1)}>Klick</button>
+    <div>
+      <h1>CARRO OCH JOHANS BRÖLLOPSFEST</h1>
+      <section>
+        hur många kommer: {count}
+        <button onClick={() => setCount(count + 1)}>Lägg till deltagare</button>
+      </section>
     </div>
   );
 }
